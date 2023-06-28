ade7a5d70b739036c5ad973437528952c49277d1
danduong17 <16079@student.vgu.edu.vn>
2023-06-29 00:47:54 +0700

    Replace hard coded data in ExpenseItem with variables for dynamic data testing

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>July 01 2023</div>
-            <div className="expense-item__description">
-                <h2 className="expense-item__title">Happy Birthday</h2>
-                <div className="expense-item__price">$200</div>
-            </div>
-        </div>
-    );
+  const expenseDate = new Date(2023, 6, 1);
+  const expenseTitle = "Happy Birthday";
+  const expenseAmount = 500;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toUTCString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item__title">{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
