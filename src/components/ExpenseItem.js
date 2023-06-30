967b9b2e320ff0acedfa7ad781e1a0b63351be7d
danduong17 <16079@student.vgu.edu.vn>
2023-06-30 15:46:23 +0700

    Separate date into a new subcomponent & add css styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   // const expenseDate = new Date(2023, 6, 1);
   // const expenseTitle = "Happy Birthday";
   // const expenseAmount = 500;
-  const month = props.date.toLocaleString("en-US", { month: "short" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2 className="expense-item__title">{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
