b1b5a8f456377f1ed7f6d57f329a20ff7566ec35
danduong17 <16079@student.vgu.edu.vn>
2023-07-05 23:21:05 +0700

    Add event listeners for user input & gathering them into 1 object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,8 @@ const ExpenseForm = () => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    // There're multiple ways to create event handler functions when input changes: 3-state (current), 1-state approach (1 state includes title, amount, date), shared event handler function
+    // Watch video 75, 76 & 77 to know more alternatives
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
         console.log(enteredTitle);
@@ -21,20 +23,43 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        /* 
+            Default browser behavior when submitting form is to reload webpage because 
+            browser actually automatically sends a request whenever a form is submitted 
+            to the server which is hosting this webpage - in this case this development server.
+            Use preventDefault() method of JavaScript to prevent the request being sent by default.
+            Since the request is not sent, the page won't reload because we stay on the currently loaded page
+            without sending any request anywhere.
+        */
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+
+        // Clear form for next input by resetting all variables to empty
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" name="title" onChange={titleChangeHandler}/>
+                    <input type="text" name="title" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" name="amount" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" name="amount" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" name="date" min="2019-01-01" onChange={dateChangeHandler}/>
+                    <input type="date" name="date" min="2019-01-01" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
