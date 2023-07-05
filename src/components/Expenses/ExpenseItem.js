a49881da43b2c63dca76ac4c8f19190f3dac1449
danduong17 <16079@student.vgu.edu.vn>
2023-07-05 10:12:12 +0700

    Try useState for changing title (practice)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,37 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  // const expenseDate = new Date(2023, 6, 1);
-  // const expenseTitle = "Happy Birthday";
-  // const expenseAmount = 500;
   
+  /* This way doesn't work because React only render all applications once no matter there is any code newly added
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Updated!";
+    console.log(title);
+  }
+
+  */
+
+  // title is variable whose initial value is originally props.title, setTitle is a function updating variable "title"  
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Finally updated!');
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2 className="expense-item__title">{props.title}</h2>
+        <h2 className="expense-item__title">{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
